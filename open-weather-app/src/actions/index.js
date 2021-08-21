efd8e8934f6c7a723c0f87a56eec5653a71ce201
otter9459 <otter9459@gmail.com>
2021-08-22 00:36:04 +0900

    MOVE_TO...MONTH action 객체 전달 추가

--- open-weather-app/src/actions/index.js
+++ open-weather-app/src/actions/index.js
@@ -1,3 +1,24 @@
 // actions type
+export const MOVE_TO_PREV_MONTH = "MOVE_TO_PREV_MONTH";
+export const MOVE_TO_NEXT_MONTH = "MOVE_TO_NEXT_MONTH";
 
-//
+// actions creator functions
+
+// 현재 today를 받아온다.
+export const moveToPrevMonth = (today) => {
+  return {
+    type: MOVE_TO_PREV_MONTH,
+    payload: {
+      today
+    }
+  }
+}
+
+export const moveToNextMonth = (today) => {
+  return {
+    type: MOVE_TO_NEXT_MONTH,
+    payload: {
+      today
+    }
+  }
+}
